a5819f38e8dd3e52196f5788abece55fa601bdaa
Anton <NotRealEmail.com>
2023-09-07 12:47:42 +0300

    added audit on entity user

--- src/main/java/com/javaKava/SpringProject/entity/User.java
+++ src/main/java/com/javaKava/SpringProject/entity/User.java
@@ -1,8 +1,14 @@
 package com.javaKava.SpringProject.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
@@ -12,15 +18,27 @@ import java.time.LocalDate;
 @Entity
 @Builder
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true, nullable = false)
     private String email;
     private String nickname;
     private LocalDate birthDate;
     private String image;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @CreatedDate
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant modifiedAt;
+
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String modifiedBy;
+
 
 }
