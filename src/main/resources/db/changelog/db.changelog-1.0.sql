334de5ca2a25b0e182a849132eb3f13f1855d1f1
Anton <NotRealEmail.com>
2023-09-07 13:43:06 +0300

    added liquibase

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+
+--changeset javaKava:1
+create table if not exists users
+(
+    id bigserial primary key,
+    email varchar not null unique,
+    nickname varchar(64),
+    image varchar,
+    role varchar(32),
+    created_at  timestamp,
+    modified_at timestamp,
+    birth_date  date,
+    created_by  varchar(32),
+    modified_by varchar(32)
+);
