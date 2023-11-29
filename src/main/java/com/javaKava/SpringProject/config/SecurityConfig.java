0811cdff60bbd18c73a912a542f9148c9b377363
Anton <NotRealEmail.com>
2023-11-29 19:33:46 +0300

    added login

---
+++ src/main/java/com/javaKava/SpringProject/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.javaKava.SpringProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll());
+
+
+        return httpSecurity.build();
+    }
+}
